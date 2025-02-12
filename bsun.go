a466ea7a14048e4d4434e57f5b96e0a31c1ef6b8
Brandon Ly <brandonly@lostcoding.com>
2025-02-12 11:06:57 -0600

    Update module name

--- bsun.go
+++ bsun.go
@@ -3,10 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 func main() {
 	name := flag.String("name", "world", "The name to greet.")
 	flag.Parse()
 	fmt.Printf("Hello, %s!\n", *name)
+}
+
+func readBson() {
+	var raw bson.Raw = nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module bsun
+module github.com/osguild/bsun
 
 go 1.21.0
+
+require go.mongodb.org/mongo-driver/v2 v2.0.0
